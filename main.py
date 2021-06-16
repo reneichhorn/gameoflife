d36e6bcf8c593b757d9035cd45a7072227eded5d
Rene Eichhorn <rene.eichhorn@drei.com>
2021-06-16 18:05:55 +0200

    Added Threading not very fast yet

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 
+from typing import List
 import pyglet
 from pyglet import shapes
 import random
+import concurrent.futures
 
-width = height = 2000
+width = 1500
+height = 1000
 window = pyglet.window.Window(width, height)
-scale = 20
+scale = 10
 cols = width // scale
 rows = height // scale
 batch = pyglet.graphics.Batch()
-width = height = width - scale
+width = width - scale
+height = height - scale
 squares = []
 DEAD = [0, 0, 0]
 ALIVE = [255, 255, 255]
 PAUSED = False
 LIVELIHOOD = 0.75
+MAXTHREADS = 300
+newsquares = []
 
 for j in range(0, rows):
     for i in range(0, cols):
@@ -31,37 +37,63 @@ def calulateNeighbours(i, j):
     aliveneighbourscount = 0
     for rowoffset in range(-1, 2):
         for coloffset in range(-1, 2):
-            if ((cols * (j + rowoffset)) + (i + coloffset)) > (len(squares) - 1) or (coloffset == 0 and rowoffset==0):
+            if aliveneighbourscount > 3:
+                break
+            lookup = (cols * (j + rowoffset)) + (i + coloffset)
+            if lookup > (len(squares) - 1) or (coloffset == 0 and rowoffset==0):
                 continue
             neighbour = squares[((cols * (j + rowoffset)) + (i + coloffset))].color
             if neighbour == ALIVE:
                 aliveneighbourscount += 1
+        if aliveneighbourscount > 3:
+            break
     return aliveneighbourscount
 
 
 def calculateNextBoard():
     global squares
-    newsquares = []
-    for square in squares:
+    futures = []
+    threadnumber = (len(squares) - 1)//MAXTHREADS
+    start = 0
+    end = threadnumber
+    # print(f'{threadnumber} fields to check per thread. {MAXTHREADS} Threads active')
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for x in range(MAXTHREADS+1):
+            squaresforthread = squares[start:end]
+            futures.append(executor.submit(calculateneighbourThreads, squaresforthread))
+            start += threadnumber
+            end = start + threadnumber
+    cleanfuture = []
+    for f in futures:
+        result = f.result()
+        if type(result) == list and len(result)>0:
+            for e in result:
+                cleanfuture.append(e)
+    newsquares = cleanfuture
+    squares = []
+    for newsquare in newsquares:
+        squares.append(shapes.Rectangle(newsquare['x'], newsquare['y'], scale-1, scale-1, color=newsquare['state'], batch=batch))
+
+
+def calculateneighbourThreads(squaresforthread):
+    threadsquares = []
+    for square in squaresforthread:
         newsquare = {}
         newsquare['state'] = square.color
         newsquare['x'] = square.x
         newsquare['y'] = square.y
 
         n = calulateNeighbours(square.x//scale, (height-square.y)//scale)
-
         if square.color == ALIVE:
             if n < 2:
                 newsquare['state'] = DEAD
             elif n > 3:
                 newsquare['state'] = DEAD
-        elif square.color == DEAD:
+        else:
             if n == 3:
                 newsquare['state'] = ALIVE
-        newsquares.append(newsquare)
-    squares = []
-    for newsquare in newsquares:
-        squares.append(shapes.Rectangle(newsquare['x'], newsquare['y'], scale-1, scale-1, color=newsquare['state'], batch=batch))
+        threadsquares.append(newsquare)
+    return threadsquares
 
 
 @window.event
@@ -99,5 +131,5 @@ def update(dt):
         calculateNextBoard()
 
 
-pyglet.clock.schedule_interval(update, 1/5)
+pyglet.clock.schedule_interval(update, 1/30)
 pyglet.app.run()
